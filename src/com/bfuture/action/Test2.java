eceedbd4ac4be8dc90594f3a8fa9c0635a0eaa91
ranzhengdong <ranzhengdong@126.com>
2013-02-18 14:52:14 +0800

    Signed-off-by: ranzhengdong <ranzhengdong@126.com>

--- src/com/bfuture/action/Test2.java
+++ src/com/bfuture/action/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("1111111111");
+		System.out.println("1111111111rrrrrrrr");
 	}
 	
 	public void Test(){
