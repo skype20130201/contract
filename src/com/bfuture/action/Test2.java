976591459484b8dee13c2f23f8f8a42c79280bc8
jerry <jerry_tsx@163.com>
2013-02-19 17:44:51 +0800

    rs

--- src/com/bfuture/action/Test2.java
+++ src/com/bfuture/action/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("1111111111rrrrrryyy");
+		System.out.println("1111111111rr9999999rrrryyy");
 	}
 	
 	public void Test(){
