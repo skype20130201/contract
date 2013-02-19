225adba160d35e5a4dbf7c0ca35e00b4100d7dfd
skype20130201 <13072731860@163.com>
2013-02-19 09:46:08 +0000

    Signed-off-by: skype20130201 <13072731860@163.com>

--- src/com/bfuture/action/Test2.java
+++ src/com/bfuture/action/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("1111111111rrrrrryyy");
+		System.out.println("1111111111rrtttttrrrryyy");
 	}
 	
 	public void Test(){
