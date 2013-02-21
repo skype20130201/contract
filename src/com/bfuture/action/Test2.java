92a1ba02a4ddcebdd1e6f038363d299da9e5d034
jerry <jerry_tsx@163.com>
2013-02-21 09:35:38 +0800

    commit;

--- src/com/bfuture/action/Test2.java
+++ src/com/bfuture/action/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("1111111111rrtttttrrrryyy");
+		System.out.println("999991rrtttttrrrryyy");
 	}
 	
 	public void Test(){
